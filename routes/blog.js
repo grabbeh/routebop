77f72bde5a58e94b4b56a376d86257c1aa750d5b
Michael <mbg@outlook.com>
2013-08-08 22:25:32 +0100

    Switched from couchdb for blog to mongo

--- routes/blog.js
+++ routes/blog.js
@@ -1,34 +1,31 @@
 
 
-//var auth = require("../config/couch.js")
-// var nano = require("nano")('http://grabbeh:everton@mbg.iriscouch.com')
-var nano = require("nano")('http://mbg.iriscouch.com')
-//var nano = require("nano")('http://127.0.0.1:5984')
+var Blog = require('../models/blog.js')
 , moment = require("moment");
 
 exports.showAll = function(req, res){
-      var blogdb = nano.use('blogs');
+     
 
-      blogdb.view('basicmap', 'basicmap', function(err, blogs){ 
+      Blog.find({}, function(err, blogs){ 
         if (err) { console.log(err)}
-          res.renderPjax("bloglist", {title: "Blogs", blogs: blogs.rows})
+          res.render("bloglist", {title: "Blogs", blogs: blogs})
   })     
 }
 
 exports.showOne = function(req, res) {
 
-  var blogdb = nano.use('blogs');
-  blogdb.get(req.params.id, { revs_info: true }, function(err, blog) {
+
+  Blog.findOne({urltitle: req.params.id}, function(err, blog) {
   if (!err)
 
-    res.renderPjax("blog", {title: blog.title, date: blog.date, body: blog.body})
+    res.render("blog", {title: blog.title, date: blog.date, body: blog.body})
 });
 
 }
 
 exports.newBlog = function(req, res){
   if (req.user._id === "grabbeh"){
-  res.renderPjax('newblog', {title: "New blog"})
+  res.render('newblog', {title: "New blog"})
 }
 else { res.send("Alas you're not allowed to post blogs on this site")}
   
@@ -36,69 +33,52 @@ else { res.send("Alas you're not allowed to post blogs on this site")}
 
 exports.post = function(req, res){
 
-var blogdb = nano.use('blogs')
 var day = moment(new Date());
 var formattedDate = day.format("MMMM Do YYYY");
 var urltitle = req.body.title.replace(/\s+/g, '-').toLowerCase();
 
-blogdb.insert({title: req.body.title, body: req.body.body, date: formattedDate}, urltitle, function(err, body, header){
-  if (err) {
-    console.log(err.message);
-    return;
-    }
-      console.log('Blog upload successful');
-      console.log(body);
-      res.redirect('/blog');
-    })
+new Blog({title: req.body.title, body: req.body.body, date: formattedDate, urltitle: urltitle}).save(function(err, blog){
+if (!err) res.redirect('/blog');
+
+})
 }
 
 exports.delete = function(req, res) {
-
-  var blogdb = nano.use('blogs');
-
-  blogdb.get(req.params.id, { revs_info: true }, function(err, blog) {    
-        console.log(blog)
-      blogdb.destroy(blog._id, blog._revs_info[0].rev, function(err, body) {
-        if (err){ console.log(err)}
-      else {
-        res.redirect('/blog/admin');
-      }
-    });
-});
-}
+     Blog.findOne({urltitle: req.params.id}, function(err, blog) {
+        
+          blog.remove();
+          res.redirect('/blog');
+        
+    })
+  };
 
 exports.edit = function(req, res) {
 
-  var blogdb = nano.use('blogs');
-  blogdb.get(req.params.id, { revs_info: true }, function(err, blog) {
+  Blog.findOne({urltitle: req.params.id}, function(err, blog) {
   if (!err){
 
-    res.renderPjax("blogedit", {title: blog.title, date: blog.date, body: blog.body, id: blog._id})
+    res.render("blogedit", {title: blog.title, date: blog.date, body: blog.body, urltitle: blog.urltitle})
     }
   });
 }
 
 exports.postEdit = function(req, res) {
+console.log(req.body);
+Blog.findOne({urltitle: req.body.urltitle}, function(err, blog){
 
-id = req.body.id;
-var blogdb = nano.use('blogs');
-blogdb.get(id, function(err, blog){
+title: req.body.title;
+body: req.body.body;
+date: req.body.date;
 
-    blogdb.insert({title: req.body.title, body: req.body.body, date: req.body.date, _rev: blog._rev}, id, function(err, body, header){
-      if (err) {
-        console.log(err.message);
-        return;
-      }
-  console.log('Blog update successful');
-  res.redirect('/blog/admin');
-  })
-})
+    blog.save(function(err){
+        res.redirect('/blog');
+      })
+    })
 }
 
 exports.admin = function(req, res){
-    var blogdb = nano.use('blogs');
 
-      blogdb.view('basicmap', 'basicmap', function(err, blogs){ 
-        res.renderPjax("blogadmin", {title: "Blogs", blogs: blogs.rows})
+      Blog.find({}, function(err, blogs){ 
+        res.render("blogadmin", {title: "Blogs", blogs: blogs})
   })     
 }
